1f463a04f23ae85e886bf047ca35d977bc66e32f
ErfanGh <erfan.ghasemian83@gmail.com>
2024-11-08 17:24:38 +0330

    fixed starting logo

--- src/script/cl.js
+++
@@ -1,3 +0,0 @@
-export function createElement(elementName, className, idName, innerHTML, innerText){
-
-}
--- src/script/main.js
+++ src/script/main.js
@@ -1,1 +1,25 @@
-document.querySelector('#app').innerHTML = '<div class="bg-red-500"> this is from js</div>'
+import {El} from "./el.js";
+import logo from '../assets/logo.svg';
+
+const header = function (){
+
+    return El({element: 'header',
+        children: [El({
+            element: 'h1',
+            children: ['Shoea']
+        }),
+    El({
+        element: 'div',
+        children: [El({
+            element: 'img',
+            className: 'w-full h-full p-2',
+            src:logo
+        })],
+        id: 'logoWrapper',
+        className: 'bg-black text-green-400 rounded-full w-12 h-12'
+    })]
+    })
+};
+
+const app = document.getElementById('app');
+app.append(header());
